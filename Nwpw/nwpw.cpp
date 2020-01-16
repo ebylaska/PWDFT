7f5c0824887753e043d9fcb26013ed7669a8670a
Bylaska <bylaska@we38802.pnl.gov>
2020-01-16 15:24:09 -0800

    ...EJB

--- Nwpw/nwpw.cpp
+++ Nwpw/nwpw.cpp
@@ -13,31 +13,9 @@ extern int cpsd(int argc, char *argv[]);
 
 int main(int argc, char* argv[])
 {
-  if (argc < 2) {
-    // report version
-    std::cout << argv[0] << " Version " << Nwpw_VERSION_MAJOR << "."
-              << Nwpw_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    std::cout << "Hello Eric"
-            << std::endl;
-    return 1;
-  }
-
   std::cout << argv[0] << " (NWChemEx) - Version " << Nwpw_VERSION_MAJOR << "."
             << Nwpw_VERSION_MINOR << std::endl;
 
-
-  // convert input to double
-  //const double inputValue = atof(argv[1]);
-  const double inputValue = std::stod(argv[1]);
-
-  // calculate square root
-  const double outputValue = sqrt(inputValue);
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
-
-  std::cout << "Hello Eric"
-            << std::endl;
 
   int ijk = cpsd(argc,argv);
 
