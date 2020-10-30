434c7dc2ff0ac2d8746675b2aa2f8359264e6428
Abhishek Bagusetty <abagusetty@anl.gov>
2020-10-30 01:16:37 +0000

    SYCL error exceptions

--- Nwpw/nwpwlib/device/gdevice.hpp
+++ Nwpw/nwpwlib/device/gdevice.hpp
@@ -9,6 +9,23 @@ extern void gdevice_NT_dgemm(int, int, int, double, double *, double *, double, 
 #ifdef NWPW_SYCL
 #include <CL/sycl.hpp>
 
+#define NWPW_SYCL_ERROR( EXPR )                                     \
+  try {                                                             \
+    EXPR;                                                           \
+  }                                                                 \
+  catch (cl::sycl::exception const &ex) {                           \
+    std::stringstream msg;                                          \
+    msg << "SYCL Exception at " << __FILE__ << " : " << __LINE__    \
+	<< std::endl;                                               \
+    throw(std::runtime_error( ex.what() ));                         \
+  }                                                                 \
+  catch(std::runtime_error const& ex) {                             \
+    std::stringstream msg;                                          \
+    msg << "runtime Exception at " << __FILE__ << " : " << __LINE__ \
+	<< std::endl;                                               \
+    throw(std::runtime_error( ex.what() ));                         \
+  }
+
 extern cl::sycl::queue* get_syclQue();
 extern int get_sycl_mem_index(const size_t memSize);
 extern double* get_sycl_mem(const int memIndex);
