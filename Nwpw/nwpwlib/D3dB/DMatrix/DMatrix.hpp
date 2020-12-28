a79a18b22f1fd74e6c825b243a076edf0841d8a5
Eric Bylaska <bylaska@gmail.com>
2020-12-28 10:56:24 -0800

    added dgemm1 and dgemm3 SUMMA

--- Nwpw/nwpwlib/D3dB/DMatrix/DMatrix.hpp
+++ Nwpw/nwpwlib/D3dB/DMatrix/DMatrix.hpp
@@ -3,7 +3,29 @@
 
 #include "Parallel.hpp"
 
+extern void DMatrix_dgemm1(int, int, int, int,
+                    double,
+                    double *, int, int *, int *,
+                    double *, int,int *,int *,
+                    double,
+                    double *, int, int *, int *,
+                    int,int,
+                    int, int,
+                    MPI_Comm, MPI_Comm,
+                    double  *, double *);
+
 extern void DMatrix_dgemm2(int, int, int, int,
+                    double,
+                    double *, int, int *, int *,
+                    double *, int,int *,int *,
+                    double,
+                    double *, int, int *, int *,
+                    int,int,
+                    int, int,
+                    MPI_Comm, MPI_Comm,
+                    double  *, double *);
+
+extern void DMatrix_dgemm3(int, int, int, int,
                     double,
                     double *, int, int *, int *,
                     double *, int,int *,int *,
