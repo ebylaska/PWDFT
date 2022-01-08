18185d77ce024dc549dad92a24817f42dd53d5f3
Eric Bylaska <bylaska@gmail.com>
2022-01-07 22:05:00 -0800

    ...EJB

--- QA/LAMMPSExample/test-lammps.cpp
+++ QA/LAMMPSExample/test-lammps.cpp
@@ -35,8 +35,8 @@ int main(int argc, char* argv[])
  
    lammps_pspw_input(MPI_COMM_WORLD, nwfilename);
 
-   uion[0] = 0.0;
-   uion[1] = 0.0;
+   uion[0] = -0.01;
+   uion[1] = 0.04;
    rion[0] = 0.0; rion[1] = 0.0; rion[2] = -2.17;
    rion[3] = 0.0; rion[4] = 0.0; rion[5] =  2.17;
    ierr += lammps_pspw_minimizer(MPI_COMM_WORLD,rion,uion,fion,qion,&E);
@@ -49,9 +49,10 @@ int main(int argc, char* argv[])
       std::cout << "uion: " << uion[0] << " " << uion[1] << std::endl;
       std::cout << std::endl;
 
-      std::cout << "fion: " << fion[0] << " " << fion[1] << " " << fion[2] << std::endl 
-                << "      " << fion[3] << " " << fion[4] << " " << fion[5] << std::endl;
-      std::cout << "qion: " << qion[0] << " " << qion[1] << std::endl;
+      std::cout << "energy=" << E << std::endl;
+      std::cout << "fion:  " << fion[0] << " " << fion[1] << " " << fion[2] << std::endl 
+                << "       " << fion[3] << " " << fion[4] << " " << fion[5] << std::endl;
+      std::cout << "qion:  " << qion[0] << " " << qion[1] << std::endl;
    }
 
 
