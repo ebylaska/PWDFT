9035cff7a78e41458b192b0acf5021cbf8408eb8
Eric Bylaska <bylaska@gmail.com>
2022-01-24 22:32:55 -0800

    adding Paw_gaunt...EJB

---
+++ Nwpw/nwpwlib/paw_utilities/Paw_gaunt.hpp
@@ -0,0 +1,30 @@
+#ifndef _PAW_GAUNT_HPP_
+#define _PAW_GAUNT_HPP_
+
+namespace pwdft {
+using namespace pwdft;
+
+
+class Paw_gaunt {
+
+public:
+   bool gaunt_iscmpx;
+   int  gaunt_lmax;
+   int  gaunt_sizel2;
+   double *gaunt_coeff;
+
+   /* Constructors */
+   Paw_gaunt(const bool, const int);
+
+   /* destructor */
+   ~Paw_gaunt() {
+      delete [] gaunt_coeff;
+   }
+
+   /* Gaunt routines */
+   double gaunt(const int, const int, const int, const int, const int, const int);
+};
+
+}
+
+#endif
