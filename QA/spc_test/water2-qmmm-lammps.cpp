91542156dd53e8229687571a0511aaba8b62093b
Eric Bylaska <bylaska@gmail.com>
2022-05-19 14:33:24 -0700

    ...EJB

--- QA/spc_test/water2-qmmm-lammps.cpp
+++ QA/spc_test/water2-qmmm-lammps.cpp
@@ -1,9 +1,13 @@
+
+#include        <iomanip>
+#include        <cstring>
 #include 	<cmath>
 #include 	<iostream>
 #include 	<fstream>
 #include 	<cstdio>
 #include 	<string>
 #include	<unistd.h>
+#include        <sys/stat.h>
 
 #include "mpi.h"
 
@@ -73,7 +77,7 @@ static void shake_chain(const int n, const int indx[], const int nb,
       done = true;
 
       for (auto a=0; a<nb; ++a) {
-         auto b = (a+1)*n;
+         auto b = (a+1)%n;
          if (moved[a] || moved[b]) {
             double pxab = pxi[a] - pxi[b];
             double pyab = pyi[a] - pyi[b];
@@ -91,6 +95,8 @@ static void shake_chain(const int n, const int indx[], const int nb,
             double pabsq  = pxab*pxab + pyab*pyab + pzab*pzab;
             double rabsq  = dsq[a];
             double diffsq = rabsq - pabsq;
+            std::cout << "a=" << a << " b=" << b << " diffsq=" << diffsq << " dsq=" << dsq[a] << " pabsq=" << pabsq << std::endl;
+
 
             if (fabs(diffsq) > (rabsq*tol2)) {
                double rxab = rxi[a] - rxi[b];
@@ -105,6 +111,7 @@ static void shake_chain(const int n, const int indx[], const int nb,
                rxab = ua[0]*cr1 + ua[3]*cr2 + ua[6]*cr3;
                ryab = ua[1]*cr1 + ua[4]*cr2 + ua[7]*cr3;
                rzab = ua[2]*cr1 + ua[5]*cr2 + ua[8]*cr3;
+               
 
                double rpab = rxab*pxab + ryab*pyab + rzab*pzab;
 
@@ -195,10 +202,6 @@ double Q_Electrostatic_potential(const double r1[], const double q1,
    double y = r2[1] - r1[1];
    double z = r2[2] - r1[2];
    double r = std::sqrt(x*x + y*y + z*z);
-
-   std::cout << "r1=" << r1[0] << " " << r1[1] << " " << r1[2] << std::endl;
-   std::cout << "r2=" << r2[0] << " " << r2[1] << " " << r2[2] << std::endl;
-   std::cout << "q1,r=" << q1 << " " << x << " " << y << " " << z << " " << r << " " << q1/r << std::endl;
 
    return (q1/r);
 }
@@ -315,15 +318,47 @@ void QMMM_electrostatic_force(const int nion_qm, const int nion,
    }
 }
 
+#define E124    std::right << std::setw(12) << std::setprecision(4)  << std::scientific
+#define E156    std::right << std::setw(15) << std::setprecision(6)  << std::scientific
+#define E1910   std::right << std::setw(19) << std::setprecision(10) << std::scientific
+#define F206    std::right << std::setw(20) << std::setprecision(6)  << std::fixed
+#define F105    std::right << std::setw(10) << std::setprecision(5)  << std::fixed
+
+#define hxyzstream(a1x,a1y,a1z,a2x,a2y,a2z,a3x,a3y,a3z) E124 << (a1x) << E124 << (a1y) << E124 << (a1z) << E124 << (a2x) <<  E124 << (a2y) << E124 << (a2z) << E124 << (a3x) << E124 << (a3y) << E124 << (a3z) 
+
+#define xyzstream(S,X,Y,Z)     std::left << std::setw(3) << (S) << E124 << (X) << E124 << (Y) << E124 << (Z) 
+
+void printxyz(std::ofstream *xyz, const int nion, const std::string symbol[], const double *unita, const double *rion) 
+{
+   double AACONV = 0.529177;
+
+   double a1x = unita[0]*AACONV;
+   double a1y = unita[1]*AACONV;
+   double a1z = unita[2]*AACONV;
+   double a2x = unita[3]*AACONV;
+   double a2y = unita[4]*AACONV;
+   double a2z = unita[5]*AACONV;
+   double a3x = unita[6]*AACONV;
+   double a3y = unita[7]*AACONV;
+   double a3z = unita[8]*AACONV;
+
+   *xyz << nion << std::endl << hxyzstream(a1x,a1y,a1z,a2x,a2y,a2z,a3x,a3y,a3z) << std::endl;
+
+   for (auto ii=0; ii<nion; ++ii)
+      *xyz << xyzstream(symbol[ii],rion[3*ii]*AACONV,rion[3*ii+1]*AACONV,rion[3*ii+2]*AACONV) << std::endl;
+}
+
+
 
 
 
 int main(int argc, char* argv[])
 {
+   std::ofstream *xyzfile;
+
    //std::string nwfilename = "w2.nw";
    std::string nwfilename = "water1.nw";
    if (argc>1) nwfilename = argv[1];
-   
 
    int ierr,np,taskid;
 
@@ -338,37 +373,48 @@ int main(int argc, char* argv[])
       std::cout << "np=" << np << " taskid=" << taskid << std::endl;
       std::cout << "argc=" << argc << std::endl;
       std::cout << "nwfilename=" << nwfilename << std::endl;
+
+      xyzfile = new (std::nothrow) std::ofstream;
+      xyzfile->open("water2qmm.xyz", std::ios::app);
    }
+
 
    int nion_qm = 3;
    int nwater  = 1;
    int nion    = nion_qm + 3*nwater;
-   double uion[nion],qion[nion],rion[3*nion],fion[3*nion];
-   double epsilon[nion],sigma[nion];
-   double E,Eqm,Ecoul,ELJ;
+   double rion0[3*nion],rion1[3*nion],rion2[3*nion];
+   double uion[nion],qion[nion],fion[3*nion];
+   double epsilon[nion],sigma[nion],mass[nion];
+   double E,KE,Eqm,Ecoul,ELJ;
+
+   std::string symbol[nion] = {"O","H","H","O","H","H"};
 
    lammps_pspw_input(MPI_COMM_WORLD, nwfilename);
 
 
    // first water molecule - QM water
-   rion[0] =  0.021259*ANGTOBOHR; rion[1] =  0.506771*ANGTOBOHR; rion[2] =  2.831278*ANGTOBOHR;
-   rion[3] = -0.721039*ANGTOBOHR; rion[4] =  1.083100*ANGTOBOHR; rion[5] =  2.758378*ANGTOBOHR;
-   rion[6] =  0.158220*ANGTOBOHR; rion[7] =  0.181883*ANGTOBOHR; rion[8] =  1.945696*ANGTOBOHR;
+   memset(rion0,0,3*nion*sizeof(double));
+   memset(rion2,0,3*nion*sizeof(double));
+   rion1[0] =  0.021259*ANGTOBOHR; rion1[1] =  0.506771*ANGTOBOHR; rion1[2] =  2.831278*ANGTOBOHR;
+   rion1[3] = -0.721039*ANGTOBOHR; rion1[4] =  1.083100*ANGTOBOHR; rion1[5] =  2.758378*ANGTOBOHR;
+   rion1[6] =  0.158220*ANGTOBOHR; rion1[7] =  0.181883*ANGTOBOHR; rion1[8] =  1.945696*ANGTOBOHR;
    qion[0] = -0.8476; qion[1] = 0.4238; qion[2] = 0.4238;
    uion[0] =  0.0000; uion[1] = 0.0000; uion[2] = 0.0000;
    sigma[0] = 3.165558*ANGTOBOHR; epsilon[0] = 0.155394/(23.06*27.2116);
    sigma[1] = 0.700000*ANGTOBOHR; epsilon[1] = 0.044000/(23.06*27.2116);
    sigma[2] = 0.700000*ANGTOBOHR; epsilon[2] = 0.044000/(23.06*27.2116);
+   mass[0] = 16.0*1822.89; mass[1]= 1.0*1822.89; mass[2]=1.0*1822.90;
 
    // second water molecule - MM water
-   rion[9]  =  0.161560*ANGTOBOHR; rion[10] = -0.052912*ANGTOBOHR; rion[11] =  0.033173*ANGTOBOHR;
-   rion[12] =  0.803054*ANGTOBOHR; rion[13] =  0.369132*ANGTOBOHR; rion[14] = -0.511660*ANGTOBOHR;
-   rion[15] = -0.325571*ANGTOBOHR; rion[16] = -0.669574*ANGTOBOHR; rion[17] = -0.488560*ANGTOBOHR;
+   rion1[9]  =  0.161560*ANGTOBOHR; rion1[10] = -0.052912*ANGTOBOHR; rion1[11] =  0.033173*ANGTOBOHR;
+   rion1[12] =  0.803054*ANGTOBOHR; rion1[13] =  0.369132*ANGTOBOHR; rion1[14] = -0.511660*ANGTOBOHR;
+   rion1[15] = -0.325571*ANGTOBOHR; rion1[16] = -0.669574*ANGTOBOHR; rion1[17] = -0.488560*ANGTOBOHR;
    qion[3]  = -0.8476; qion[4] = 0.4238; qion[5] = 0.4238;
    uion[3]  =  0.0000; uion[4] = 0.0000; uion[5] = 0.0000;
    sigma[3] = 3.165558*ANGTOBOHR; epsilon[3] = 0.155394/(23.06*27.2116);
    sigma[4] = 0.700000*ANGTOBOHR; epsilon[4] = 0.0;
    sigma[5] = 0.700000*ANGTOBOHR; epsilon[5] = 0.0;
+   mass[3] = 16.0*1822.89; mass[4]= 1.0*1822.89; mass[5]=1.0*1822.90;
 
 
    //lj_ion_parameters O 3.165558 0.155394
@@ -376,41 +422,140 @@ int main(int argc, char* argv[])
    //lj_ion_parameters O^ 3.165558 0.155394a
 
    //shake units angstroms 1 2 3 cyclic 1.0 1.632993125 1.0
+   double unita[9] = {20.0,  0.0,  0.0,
+                       0.0, 20.0,  0.0,
+                       0.0,  0.0, 20.0};
    double dsq[3] = {1.0*ANGTOBOHR, 1.632993125*ANGTOBOHR, 1.0*ANGTOBOHR};
+   int mm_water_indx[3] = {3, 4, 5};
+   double dt = 5.0;
 
    // zero potentials and forces 
    memset(uion,0,nion*sizeof(double));
    memset(fion,0,3*nion*sizeof(double));
 
    // calculate electrostatic potential on QM atoms, u(ii)
-   QMMM_electrostatic_potential(nion_qm,nion,qion,rion,uion);
+   QMMM_electrostatic_potential(nion_qm,nion,qion,rion1,uion);
 
    // QM energy and forces
-   ierr += lammps_pspw_qmmm_minimizer(MPI_COMM_WORLD,rion,uion,fion,qion,&Eqm);
+   ierr += lammps_pspw_qmmm_minimizer(MPI_COMM_WORLD,rion1,uion,fion,qion,&Eqm);
 
    // QMMM Electrostatic energy and forces
    Ecoul = 0.0;
    for (auto ii=0; ii<nion_qm; ++ii)
       Ecoul += qion[ii]*uion[ii];
-   QMMM_electrostatic_force(nion_qm,nion,qion,rion,fion);
+   QMMM_electrostatic_force(nion_qm,nion,qion,rion1,fion);
 
    // QMMM Electrostatic energy and forces
-   ELJ = QMMM_LJ_energy(nion_qm,nion,epsilon,sigma,rion);
-   QMMM_LJ_force(nion_qm,nion,epsilon,sigma,rion,fion);
+   ELJ = QMMM_LJ_energy(nion_qm,nion,epsilon,sigma,rion1);
+   QMMM_LJ_force(nion_qm,nion,epsilon,sigma,rion1,fion);
 
+   // take a Newton step
+   for (auto ii=0; ii<nion; ++ii)
+   {
+      rion2[3*ii]   = rion1[3*ii]   + dt*rion0[3*ii]   + 0.5*(dt*dt/mass[ii])*fion[3*ii];
+      rion2[3*ii+1] = rion1[3*ii+1] + dt*rion0[3*ii+1] + 0.5*(dt*dt/mass[ii])*fion[3*ii+1];
+      rion2[3*ii+2] = rion1[3*ii+2] + dt*rion0[3*ii+2] + 0.5*(dt*dt/mass[ii])*fion[3*ii+2];
+   }
+
+   // shake MM water
+   shake_chain(3,mm_water_indx,3,1.0e-6,25,dsq,mass,unita,rion1,rion2); 
+
+   // kinetic energy
+   KE = 0.0;
+   for (auto ii=0; ii<nion; ++ii)
+      KE += 0.5*mass[ii]*(rion0[3*ii]*rion0[3*ii] + rion0[3*ii+1]*rion0[3*ii+1] + rion0[3*ii+2]*rion0[3*ii+2]);
+
+   // Current step output
    if (taskid==MASTER)
    {
       std::cout << std::endl << std::endl;
       for (auto ii=0; ii<nion; ++ii)
-         std::cout << "ii=" << ii << " rion: "  << rion[3*ii]   << " " << rion[3*ii+1]   << " " << rion[3*ii+2]   << " uion=" << uion[ii]   << std::endl;
+         std::cout << "ii=" << ii << " rion: "  << rion1[3*ii]   << " " << rion1[3*ii+1]   << " " << rion1[3*ii+2]   << " uion=" << uion[ii]   << std::endl;
 
-      std::cout << "energy=" << Eqm+Ecoul+ELJ << " Eqm=" << Eqm << " Ecoul=" << Ecoul << " ELJ=" << ELJ << std::endl;
+      std::cout << "KE+energy=" << KE+Eqm+Ecoul+ELJ << " energy=" << Eqm+Ecoul+ELJ << " KE=" << KE << " Eqm=" << Eqm << " Ecoul=" << Ecoul << " ELJ=" << ELJ << std::endl;
 
       for (auto ii=0; ii<nion; ++ii)
          std::cout << "ii=" << ii << " fion: "  << fion[3*ii]   << " " << fion[3*ii+2]   << " " << fion[3*ii+2]   << " qion=" << qion[ii]   << std::endl;
       std::cout << std::endl << std::endl;
+
+      printxyz(xyzfile,nion,symbol,unita,rion1) ;
    }
 
+
+
+
+   // Verlet Iterations 
+   for(auto it=0; it<1; ++it)
+   {
+      for (auto ii=0; ii<(3*nion); ++ii) rion0[ii] = rion1[ii];
+      for (auto ii=0; ii<(3*nion); ++ii) rion1[ii] = rion2[ii];
+
+      // zero potentials and forces 
+      memset(uion,0,nion*sizeof(double));
+      memset(fion,0,3*nion*sizeof(double));
+
+      // calculate electrostatic potential on QM atoms, u(ii)
+      QMMM_electrostatic_potential(nion_qm,nion,qion,rion1,uion);
+
+      // QM energy and forces
+      ierr += lammps_pspw_qmmm_minimizer(MPI_COMM_WORLD,rion1,uion,fion,qion,&Eqm);
+
+      // QMMM Electrostatic energy and forces
+      Ecoul = 0.0;
+      for (auto ii=0; ii<nion_qm; ++ii)
+         Ecoul += qion[ii]*uion[ii];
+      QMMM_electrostatic_force(nion_qm,nion,qion,rion1,fion);
+
+      // QMMM Electrostatic energy and forces
+      ELJ = QMMM_LJ_energy(nion_qm,nion,epsilon,sigma,rion1);
+      QMMM_LJ_force(nion_qm,nion,epsilon,sigma,rion1,fion);
+
+
+      // take a position Verlet step
+      for (auto ii=0; ii<nion; ++ii)
+      {
+         rion2[3*ii]   = 2.0*rion1[3*ii]   - rion0[3*ii]   + (dt*dt/mass[ii])*fion[3*ii];
+         rion2[3*ii+1] = 2.0*rion1[3*ii+1] - rion0[3*ii+1] + (dt*dt/mass[ii])*fion[3*ii+1];
+         rion2[3*ii+2] = 2.0*rion1[3*ii+2] - rion0[3*ii+2] + (dt*dt/mass[ii])*fion[3*ii+2];
+      }
+
+      // shake MM water
+      shake_chain(3,mm_water_indx,3,1.0e-6,25,dsq,mass,unita,rion1,rion2); 
+
+
+      // kinetic energy
+      for (auto ii=0; ii<(3*nion); ++ii) 
+         rion0[ii] = (rion2[ii]-rion0[ii])/(2.0*dt);
+      KE = 0.0;
+      for (auto ii=0; ii<nion; ++ii)
+      {
+         KE += 0.5*mass[ii]*(rion0[3*ii]*rion0[3*ii] + rion0[3*ii+1]*rion0[3*ii+1] + rion0[3*ii+2]*rion0[3*ii+2]);
+         std::cout << ii << " mass, ke=" << mass[ii] << " " << KE << std::endl;
+      }
+
+      if (taskid==MASTER)
+      {
+         std::cout << std::endl << std::endl;
+         for (auto ii=0; ii<nion; ++ii)
+            std::cout << "ii=" << ii << " rion: "  << rion1[3*ii]   << " " << rion1[3*ii+1]   << " " << rion1[3*ii+2]   
+                                     << " vion: "  << rion0[3*ii]   << " " << rion0[3*ii+1]   << " " << rion0[3*ii+2]   
+                      << " uion=" << uion[ii]   << std::endl;
+
+         std::cout << "KE+energy=" << KE+Eqm+Ecoul+ELJ << " energy=" << Eqm+Ecoul+ELJ << " KE=" << KE << " Eqm=" << Eqm << " Ecoul=" << Ecoul << " ELJ=" << ELJ << std::endl;
+
+         for (auto ii=0; ii<nion; ++ii)
+            std::cout << "ii=" << ii << " fion: "  << fion[3*ii]   << " " << fion[3*ii+2]   << " " << fion[3*ii+2]   << " qion=" << qion[ii]   << std::endl;
+         std::cout << std::endl << std::endl;
+
+         printxyz(xyzfile,nion,symbol,unita,rion1) ;
+      }
+   }
+
+   if (taskid==MASTER)
+   {
+      xyzfile->close();
+      delete xyzfile;
+   }
 
 
 
