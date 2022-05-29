ad626d36bee3a5b634215b1b6f185a6c805a7301
Eric Bylaska <bylaska@gmail.com>
2022-05-28 18:11:17 -0700

    added switching function...EJB

---
+++ Nwpw/nwpwlib/utilities/util_radial_switching.hpp
@@ -0,0 +1,12 @@
+#ifndef _util_RADIAL_SWITCHING_HPP_
+#define _util_RADIAL_SWITCHING_HPP_
+
+namespace pwdft {
+using namespace pwdft;
+
+extern double util_radial_switching(const double, const double, const double);
+extern void  util_radial_dswitching(const double, const double, const double, double *, double *);
+
+}
+
+#endif
