71f9d20c317d92fbd3755150870f6a2ddf18595e
Eric Bylaska <bylaska@gmail.com>
2022-07-28 13:49:33 -0700

    added radix-8

--- Miscellaneous/programfft/stock1.cpp
+++ Miscellaneous/programfft/stock1.cpp
@@ -12,7 +12,7 @@ void fft0_fac2(int isgn, int n, int s, bool eo, complex_t* x, complex_t* y)
 // x  : input sequence(or output sequence if eo == 0)
 // y  : work area(or output sequence if eo == 1)
 {
-    std::cout << "n=" << n << " s=" << s << " eo=" << eo  << std::endl;
+    //std::cout << "n=" << n << " s=" << s << " eo=" << eo  << std::endl;
     const int m = n/2;
     const double theta0 = 2*M_PI/n;
 
@@ -86,9 +86,9 @@ void fft0_fac4(int isgn, int n, int s, bool eo, complex_t* x, complex_t* y)
               const complex_t c = x[q + s*(p + 2*m)];
               const complex_t d = x[q + s*(p + 3*m)];
               y[q + s*(4*p + 0)] =  a + b + c + d;
-              y[q + s*(4*p + 1)] = (a + b*u14 - c     + d*u34) * wp;
-              y[q + s*(4*p + 2)] = (a - b     + c     - d    ) * wp2;
-              y[q + s*(4*p + 3)] = (a + b*u34 - c     + d*u14) * wp3;
+              y[q + s*(4*p + 1)] = (a + b*u14 + c*u24 + d*u34) * wp;
+              y[q + s*(4*p + 2)] = (a + b*u24 + c     + d*u24) * wp2;
+              y[q + s*(4*p + 3)] = (a + b*u34 + c*u24 + d*u14) * wp3;
           }
       }
       //fft0_fac4(n/4, 4*s, !eo, y, x);
@@ -180,6 +180,58 @@ void fft0_fac7(int isgn, int n, int s, bool eo, complex_t* x, complex_t* y)
 }
 
 
+void fft0_fac8(int isgn, int n, int s, bool eo, complex_t* x, complex_t* y)
+// isgn: -1 forward fft, 1 inverse fft
+// n  : sequence length
+// s  : stride
+// eo : x is output if eo == 0, y is output if eo == 1
+// x  : input sequence(or output sequence if eo == 0)
+// y  : work area(or output sequence if eo == 1)
+{  
+   const int m = n/8;
+   const double theta0 = 2*M_PI/n;
+   const complex_t u18 = complex_t(cos( 2*M_PI/8.0), isgn*sin( 2*M_PI/8.0)); //  sqrt2/2 + i*sqrt2/2
+   const complex_t u28 = complex_t(cos( 4*M_PI/8.0), isgn*sin( 4*M_PI/8.0)); //  i
+   const complex_t u38 = complex_t(cos( 6*M_PI/8.0), isgn*sin( 6*M_PI/8.0)); // -sqrt2/2 + i*sqrt2/2
+   const complex_t u48 = complex_t(cos( 8*M_PI/8.0), isgn*sin( 8*M_PI/8.0)); // -1
+   const complex_t u58 = complex_t(cos(10*M_PI/8.0), isgn*sin(10*M_PI/8.0)); // -sqrt2/2 - i*sqrt2/2
+   const complex_t u68 = complex_t(cos(12*M_PI/8.0), isgn*sin(12*M_PI/8.0)); // -i
+   const complex_t u78 = complex_t(cos(14*M_PI/8.0), isgn*sin(14*M_PI/8.0)); // sqrt2/2 - i*sqrt2/2
+   if (n == 1) { if (eo) for (int q = 0; q < s; q++) y[q] = x[q]; }
+   else { 
+      for (int p = 0; p < m; p++) {
+          const complex_t wp  = complex_t(cos(p*theta0),   isgn*sin(p*theta0));
+          const complex_t wp2 = complex_t(cos(2*p*theta0), isgn*sin(2*p*theta0));
+          const complex_t wp3 = complex_t(cos(3*p*theta0), isgn*sin(3*p*theta0));
+          const complex_t wp4 = complex_t(cos(4*p*theta0), isgn*sin(4*p*theta0));
+          const complex_t wp5 = complex_t(cos(5*p*theta0), isgn*sin(5*p*theta0));
+          const complex_t wp6 = complex_t(cos(6*p*theta0), isgn*sin(6*p*theta0));
+          const complex_t wp7 = complex_t(cos(7*p*theta0), isgn*sin(7*p*theta0));
+          for (int q = 0; q < s; q++) { 
+              const complex_t a = x[q + s*(p + 0)];
+              const complex_t b = x[q + s*(p + 1*m)];
+              const complex_t c = x[q + s*(p + 2*m)];
+              const complex_t d = x[q + s*(p + 3*m)];
+              const complex_t e = x[q + s*(p + 4*m)];
+              const complex_t f = x[q + s*(p + 5*m)];
+              const complex_t g = x[q + s*(p + 6*m)]; 
+              const complex_t h = x[q + s*(p + 7*m)]; 
+              y[q + s*(8*p + 0)] =  a + b + c + d + e + f + g + h;
+              y[q + s*(8*p + 1)] = (a + b*u18 + c*u28 + d*u38 + e*u48 + f*u58 + g*u68 + h*u78) * wp;
+              y[q + s*(8*p + 2)] = (a + b*u28 + c*u48 + d*u68 + e     + f*u28 + g*u48 + h*u68) * wp2;
+              y[q + s*(8*p + 3)] = (a + b*u38 + c*u68 + d*u18 + e*u48 + f*u78 + g*u28 + h*u58) * wp3;
+              y[q + s*(8*p + 4)] = (a + b*u48 + c     + d*u48 + e     + f*u48 + g     + h*u48) * wp4;
+              y[q + s*(8*p + 5)] = (a + b*u58 + c*u28 + d*u78 + e*u48 + f*u18 + g*u68 + h*u38) * wp5;
+              y[q + s*(8*p + 6)] = (a + b*u68 + c*u48 + d*u28 + e     + f*u68 + g*u48 + h*u28) * wp6;
+              y[q + s*(8*p + 7)] = (a + b*u78 + c*u68 + d*u58 + e*u48 + f*u38 + g*u28 + h*u18) * wp7;
+          }
+      }
+      //fft0_fac8(n/8, 8*s, !eo, y, x);
+   }
+}
+
+
+
 
 
 
@@ -193,9 +245,20 @@ void fft(int isgn, int n, complex_t* x) // Fourier transform
     int s  = 1;
     int nn = n;
     while (s<=n) {
+       std::cout << "nn=" << nn << " s=" << s << " eo=" << eo  << std::endl;
 
-       if ((nn%7)==0)
+       if ((nn%8)==0)
        {
+          std::cout << " - radix-8 " << std::endl;
+          if (eo)
+             fft0_fac8(isgn,nn, s, eo, y, x);
+          else
+             fft0_fac8(isgn,nn, s, eo, x, y);
+          nn /= 8; s *= 8; eo = !eo;
+       }
+       else if ((nn%7)==0)
+       {
+          std::cout << " - radix-7 " << std::endl;
           if (eo)
              fft0_fac7(isgn,nn, s, eo, y, x);
           else
@@ -204,14 +267,25 @@ void fft(int isgn, int n, complex_t* x) // Fourier transform
        }
        else if ((nn%5)==0)
        {
+          std::cout << " - radix-5 " << std::endl;
           if (eo)
              fft0_fac5(isgn,nn, s, eo, y, x);
           else
              fft0_fac5(isgn,nn, s, eo, x, y);
           nn /= 5; s *= 5; eo = !eo;
        }
+       else if ((nn%4)==0)
+       {
+          std::cout << " - radix-4 "<< std::endl;
+          if (eo)
+             fft0_fac4(isgn,nn, s, eo, y, x);
+          else
+             fft0_fac4(isgn,nn, s, eo, x, y);
+          nn /= 4; s *= 4; eo = !eo;
+       }
        else if ((nn%3)==0)
        {
+          std::cout << " - radix-3 " << std::endl;
           if (eo)
              fft0_fac3(isgn,nn, s, eo, y, x);
           else
@@ -220,6 +294,7 @@ void fft(int isgn, int n, complex_t* x) // Fourier transform
        }
        else
        {
+          std::cout << " - radix-2 " << std::endl;
           //fft0_fac2(n, 1, 0, x, y);
           if (eo)
              fft0_fac2(isgn,nn, s, eo, y, x);
@@ -245,7 +320,7 @@ void fft(int isgn, int n, complex_t* x) // Fourier transform
 }
 */
 
-#define	N 21
+#define	N 40
 int main()
 {
    complex_t x[N];
@@ -260,6 +335,7 @@ int main()
    // forward fft
    fft(-1,N,x);
 
+   std::cout << std::endl << std::endl;
    // backward fft
    fft(1,N,x);
 
