ec2f6a78767516777c6850bebbe47cf9c1c33e30
Eric Bylaska <bylaska@gmail.com>
2023-04-26 11:13:12 -0700

    ...EJB

--- Nwpw/nwpwlib/psp_library/NwpwLibrarypsConfig.hpp
+++
@@ -1,6 +0,0 @@
-// the configured options and settings for Tutorial
-#define Nwpw_VERSION_MAJOR 1
-#define Nwpw_VERSION_MINOR 0
-#define Nwpw_COMPILE_TIMESTAMP  "Sun Mar 19 12:37:33 2023"
-#define Nwpw_TOP "/Users/bylaska/Codes/PWDFT/Nwpw"
-#define Nwpw_LIBRARYPS_Default "/Users/bylaska/Codes/PWDFT/Nwpw/libraryps"
