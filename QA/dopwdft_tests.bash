14858459a28184178128c5002e29a3f19c1f2c4f
Eric Bylaska <bylaska@gmail.com>
2023-07-08 18:22:06 -0700

    ...EJB

--- QA/dopwdft_tests.bash
+++ QA/dopwdft_tests.bash
@@ -8,5 +8,6 @@ fi
 
 echo "Running QA tests: NPROCS="$NPROCS
 echo " "
+
 ./runtest.bash -n $NPROCS C2 benzene
 ./runtest.bash -n $NPROCS ch3cl ccl4_born_test periodic_polarizability aperiodic_polarizability
