32c3a6db1437005f39b05052881ea6b7a7a3c13d
Eric Bylaska <bylaska@gmail.com>
2023-07-08 18:38:52 -0700

    ...EJB

--- QA/dopwdft_tests.bash
+++ QA/dopwdft_tests.bash
@@ -6,18 +6,18 @@ if [ -z "${NPROCS}" ];  then
    NPROCS=2
 fi
 
-echo "Running QA tests: NPROCS="$NPROCS
+echo "Running QA/tests: NPROCS="$NPROCS
 echo " "
 
-echo "Short tests:"
+echo "Short QA/tests:"
 echo " "
 ./runtest.bash -n $NPROCS C2
 
 echo " "
-echo "Medium tests:"
+echo "Medium QA/tests:"
 echo " "
 ./runtest.bash -n $NPROCS benzene ch3cl ccl4_born_test periodic_polarizability aperiodic_polarizability
 
 echo " "
-echo "Long tests:"
+echo "Long QA/tests:"
 echo " "
