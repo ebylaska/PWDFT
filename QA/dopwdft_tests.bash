dc09dc05e724c150742c23d5a40ed3e21e351f2b
Eric Bylaska <bylaska@gmail.com>
2023-07-08 18:12:11 -0700

    ...EJB

--- QA/dopwdft_tests.bash
+++ QA/dopwdft_tests.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash -f
 #
+# HOW TO RUN in BASH: export NPROCS=3; ./dopwdft_tests.bash
 #
-echo "Running QA tests:"
-echo
-./runtest.bash -n 2 C2 benzene
-./runtest.bash -n 2 ch3cl ccl4_born_test
+if [ -z "${NPROCS}" ];  then
+   NPROCS=2
+fi
+
+echo "Running QA tests: NPROCS="$NPROCS
+echo " "
+./runtest.bash -n $NPROCS C2 benzene
+./runtest.bash -n $NPROCS ch3cl ccl4_born_test periodic_polarizability aperiodic_polarizability
