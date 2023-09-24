8087152423e9c86eeab5124bace5ab978939369d
Eric Bylaska <bylaska@gmail.com>
2023-09-24 08:59:34 -0700

    adding comments...EJB

--- Nwpw/nwpwlib/lattice/Lattice.cpp
+++ Nwpw/nwpwlib/lattice/Lattice.cpp
@@ -11,6 +11,11 @@
  * Last modified: 5/9/2023
  */
 
+/**
+ * @class Lattice
+ * @brief Represents a lattice in a DFT simulation.
+ */
+
 #include "Lattice.hpp"
 #include "Control2.hpp"
 #include <cmath>
@@ -36,6 +41,20 @@ namespace pwdft {
  *     unitg : pointer to a 3x3 matrix to store the reciprocal lattice vectors (output)
  *     omega : pointer to a variable to store the volume of the unit cell (output)
  */
+/**
+ * @brief Calculate the volume and reciprocal lattice vectors of a unit cell described by lattice vectors.
+ *
+ * This static function calculates the volume and the reciprocal lattice vectors of a unit cell described by a 3x3 matrix
+ * of lattice vectors 'unita'. The reciprocal lattice vectors are returned in 'unitg' as a 3x3 matrix,
+ * and the volume of the unit cell is returned in 'omega'.
+ *
+ * The function uses the formula for the reciprocal lattice vectors derived from the cross products of the lattice vectors
+ * and scales the vectors by 2*pi/volume to get the correct units.
+ *
+ * @param unita Pointer to a 3x3 matrix of lattice vectors (input).
+ * @param unitg Pointer to a 3x3 matrix to store the reciprocal lattice vectors (output).
+ * @param omega Pointer to a variable to store the volume of the unit cell (output).
+ */
 static void get_cube(double *unita, double *unitg, double *omega) {
    double twopi = 8.0*std::atan(1.0);
    unitg[0] = unita[4]*unita[8] - unita[5]*unita[7];
@@ -53,6 +72,22 @@ static void get_cube(double *unita, double *unitg, double *omega) {
    *omega = fabs(volume);
 }
 
+ 
+/********************************
+ *                              *
+ *            get_ub            *
+ *                              *
+ ********************************/
+/**
+ * @brief Calculate the reciprocal lattice vectors (ub) from the direct lattice vectors (unita).
+ *
+ * This static function calculates the reciprocal lattice vectors (ub) from the direct lattice vectors (unita).
+ * The calculated reciprocal lattice vectors are stored in the `ub` array. The function also calculates
+ * the volume of the unit cell and normalizes the reciprocal lattice vectors.
+ *
+ * @param unita Pointer to the array of direct lattice vectors (unita).
+ * @param ub Pointer to the array to store the calculated reciprocal lattice vectors (ub).
+ */
 static void get_ub(double *unita, double *ub)
 {
    ub[0] = unita[4]*unita[8] - unita[5]*unita[7];
@@ -70,7 +105,6 @@ static void get_ub(double *unita, double *ub)
 }
 
 
-
 /********************************
  *                              *
  *         Constructors         *
@@ -83,6 +117,16 @@ static void get_ub(double *unita, double *ub)
  * object. It also calculates some useful values, such as the energy cutoff, 
  * the width cutoff, and whether the calculation is periodic or not.            
  */                                                 
+/**
+ * @brief Constructor for the Lattice class.
+ *
+ * This constructor initializes the Lattice class instance using a Control2 object. It sets the lattice vectors,
+ * the reciprocal lattice vectors, and the unit cell volume, based on the input parameters of the Control2 object.
+ * It also calculates some useful values, such as the energy cutoff, the width cutoff, and whether the calculation is
+ * periodic or not.
+ *
+ * @param control A Control2 object containing input parameters.
+ */
 Lattice::Lattice(Control2 &control) 
 {
    int nx, ny, nz, nxh, nyh, nzh;
@@ -139,9 +183,10 @@ Lattice::Lattice(Control2 &control)
    paperiodic = (control.version == 4);
 }
 
+
 /********************************
  *                              *
- *           abc_abg            *
+ *       Lattice::abc_abg       *
  *                              *
  ********************************/
 /* This function calculates the lattice parameters (a, b, c) and angles (alpha, beta, gamma) 
@@ -149,6 +194,19 @@ Lattice::Lattice(Control2 &control)
  * values accordingly. The function uses trigonometric equations to calculate the angles and 
  * the Euclidean distance formula to calculate the lengths of the unit cell vectors.
  * The angles are returned in degrees.
+ */
+/**
+ * @brief Calculate lattice parameters and angles.
+ *
+ * This function calculates the lattice parameters (a, b, c) and angles (alpha, beta, gamma) 
+ * from the unit cell vectors. The calculated angles are returned in degrees.
+ *
+ * @param a1 Pointer to store the lattice parameter 'a'.
+ * @param b1 Pointer to store the lattice parameter 'b'.
+ * @param c1 Pointer to store the lattice parameter 'c'.
+ * @param alpha1 Pointer to store the angle 'alpha' in degrees.
+ * @param beta1 Pointer to store the angle 'beta' in degrees.
+ * @param gamma1 Pointer to store the angle 'gamma' in degrees.
  */
 void Lattice::abc_abg(double *a1, double *b1, double *c1,
                       double *alpha1, double *beta1, double *gamma1) 
@@ -185,9 +243,10 @@ void Lattice::abc_abg(double *a1, double *b1, double *c1,
    *gamma1 = gamma;
 }
 
+
 /********************************
  *                              *
- *         min_diff_xyz         *
+ *     Lattice::min_diff_xyz    *
  *                              *
  ********************************/
 void Lattice::min_diff_xyz(double *x, double *y, double *z)
@@ -205,9 +264,17 @@ void Lattice::min_diff_xyz(double *x, double *y, double *z)
 
 /********************************
  *                              *
- *           min_diff           *
+ *       Lattice::min_diff      *
  *                              *
  ********************************/
+/**
+ * @brief Computes minimum differences in coordinates.
+ * 
+ * This function computes the minimum differences in coordinates using the given
+ * periodic boundary conditions (if enabled).
+ * 
+ * @param rxyz Array representing the coordinates.
+ */
 void Lattice::min_diff(double *rxyz)
 {
    if (!paperiodic)
