a14d72d4e8c07733cf9cdf6e56f1c59d1942b939
eric bylaska <eric.bylaska@pnnl.gov>
2024-07-17 16:14:24 -0700

    Checkpoint #1 for band

---
+++ Nwpw/nwpwlib/psp_library/NwpwLibrarypsConfig.hpp
@@ -0,0 +1,6 @@
+// the configured options and settings for Tutorial
+#define Nwpw_VERSION_MAJOR 1
+#define Nwpw_VERSION_MINOR 0
+#define Nwpw_COMPILE_TIMESTAMP  "Mon Jul 15 16:29:01 2024"
+#define Nwpw_TOP "/Users/bylaska/Codes/PWDFT/Nwpw"
+#define Nwpw_LIBRARYPS_Default "/Users/bylaska/Codes/PWDFT/Nwpw/libraryps"
