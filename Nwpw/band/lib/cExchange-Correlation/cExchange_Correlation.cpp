d4595c1e7e373e977a1f858ffc4b81b7445f4b5c
eric bylaska <eric.bylaska@pnnl.gov>
2025-11-08 17:57:52 -0800

    adding vdw and grimme options to xc...EJB

--- Nwpw/band/lib/cExchange-Correlation/cExchange_Correlation.cpp
+++ Nwpw/band/lib/cExchange-Correlation/cExchange_Correlation.cpp
@@ -36,6 +36,21 @@
  *   gga = 304 m06-l
  *   gga = 305  m06
  *   gga = 306  m06-2x
+ *
+ *   **** control dispersion block ****
+ *   has_disp = false
+ *   is_grimme2 = false
+ *   has_vdw  = false
+ *   is_vdw2 = false
+ *   options_disp = ''
+ *  
+ *   std::string xc_name,options_disp;
+ *   int gga;
+ *   bool use_lda, use_gga, use_mgga;
+ *
+ *   bool has_disp = false;
+ *   bool has_vdw  = false;
+ *   bool is_grimme2,is_vdw2;
  */
 
 #include "cExchange_Correlation.hpp"
@@ -59,27 +74,37 @@ cXC_Operator::cXC_Operator(Cneb *mygrid, Control2 &control)
    mycneb = mygrid;
    xc_name = control.xc_name();
  
+   // set the grimme and vdw options
+   if      (mystring_contains(mystring_lowercase(xc_name), "-grimme2")) { has_disp = true; is_grimme2 = true;  options_disp = " -old -noprint";}
+   else if (mystring_contains(mystring_lowercase(xc_name), "-grimme3")) { has_disp = true; is_grimme2 = false; options_disp = " -zero -noprint";}
+   else if (mystring_contains(mystring_lowercase(xc_name), "-grimme4")) { has_disp = true; is_grimme2 = false; options_disp = " -bj -num -noprint";}
+   else if (mystring_contains(mystring_lowercase(xc_name), "-grimme5")) { has_disp = true; is_grimme2 = false; options_disp = " -zerom -noprint";}
+   else if (mystring_contains(mystring_lowercase(xc_name), "-grimme6")) { has_disp = true; is_grimme2 = false; options_disp = " -bjm -num -noprint";}
+   else if (mystring_contains(mystring_lowercase(xc_name), "-vdw2"))    { has_vdw = true; is_vdw2 = false; }
+   else if (mystring_contains(mystring_lowercase(xc_name), "-vdw"))     { has_vdw = true; is_vdw2 = true; }
+
    gga = 0;
  
    if (mystring_contains(mystring_lowercase(xc_name), "vosko")) gga = 0;
    if (mystring_contains(mystring_lowercase(xc_name), "lda"))   gga = 0;
 
-   if (mystring_contains(mystring_lowercase(xc_name), "pbe"))        gga = 10;
-   if (mystring_contains(mystring_lowercase(xc_name), "pbe96"))      gga = 10;
-   if (mystring_contains(mystring_lowercase(xc_name), "blyp"))       gga = 11;
-   if (mystring_contains(mystring_lowercase(xc_name), "revpbe"))     gga = 12;
-   if (mystring_contains(mystring_lowercase(xc_name), "pbesol"))     gga = 13;
-   if (mystring_contains(mystring_lowercase(xc_name), "hser"))       gga = 14;
-   if (mystring_contains(mystring_lowercase(xc_name), "b3lypr"))     gga = 15;
-   if (mystring_contains(mystring_lowercase(xc_name), "beef"))       gga = 16;
-   if (mystring_contains(mystring_lowercase(xc_name), "xbeef-cpbe")) gga = 17;
-   
-   if (mystring_contains(mystring_lowercase(xc_name), "pbe0"))    gga = 110;
-   if (mystring_contains(mystring_lowercase(xc_name), "blyp0"))   gga = 111;
-   if (mystring_contains(mystring_lowercase(xc_name), "revpbe0")) gga = 112;
-   if (mystring_contains(mystring_lowercase(xc_name), "bnl"))     gga = 113;
-   if (mystring_contains(mystring_lowercase(xc_name), "hse"))     gga = 114;
-   if (mystring_contains(mystring_lowercase(xc_name), "b3lyp"))   gga = 115;
+   // set the gga options
+   if (mystring_contains(mystring_lowercase(xc_name), "pbe"))        {gga = 10; if (has_disp) options_disp = "-func pbe" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "pbe96"))      {gga = 10; if (has_disp) options_disp = "-func pbe" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "blyp"))       {gga = 11; if (has_disp) options_disp = "-func b-lyp" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "revpbe"))     {gga = 12; if (has_disp) options_disp = "-func revpbe" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "pbesol"))     {gga = 13; if (has_disp) options_disp = "-func pbesol" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "hser"))       {gga = 14; if (has_disp) options_disp = "-func hse06" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "b3lypr"))     {gga = 15; if (has_disp) options_disp = "-func b3-lyp" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "beef"))       {gga = 16; if (has_disp) options_disp = "-func pbesol" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "xbeef-cpbe")) {gga = 17; if (has_disp) options_disp = "-func pbesol" + options_disp;}
+
+   if (mystring_contains(mystring_lowercase(xc_name), "pbe0"))    {gga = 110; if (has_disp) options_disp = "-func pbe0" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "blyp0"))   {gga = 111; if (has_disp) options_disp = "-func b3-lyp" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "revpbe0")) {gga = 112; if (has_disp) options_disp = "-func revpbe0" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "bnl"))     {gga = 113; if (has_disp) options_disp = "-func hse06" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "hse"))     {gga = 114; if (has_disp) options_disp = "-func hse06" + options_disp;}
+   if (mystring_contains(mystring_lowercase(xc_name), "b3lyp"))   {gga = 115; if (has_disp) options_disp = "-func b3-lyp" + options_disp;}
 
    if (mystring_contains(mystring_lowercase(xc_name), "hartree-fock")) gga = 200;
    if (mystring_contains(mystring_lowercase(xc_name), "hf"))           gga = 200;
--- Nwpw/pspw/lib/psp/Psp_U.cpp
+++ Nwpw/pspw/lib/psp/Psp_U.cpp
@@ -23,11 +23,93 @@
 #include "blas.h"
 
 #include "Psp_U.hpp"
+#include "Paw_gaunt.hpp"
 #include "compressed_io.hpp"
 
 namespace pwdft {
 
 
+/****************************************************
+ *                                                  *
+ *                uterm_gen_vmmmm                *
+ *                                                  *
+ ****************************************************/
+static void uterm_gen_vmmmm(int l,int lm, double U, double J, double *vmmmm)
+{
+   double fourpi = 16.0*atan(1.0);
+   double F[4];
+
+   if (l==0) 
+   {
+      F[0] = U;
+      F[1] = 0.0;
+      F[2] = 0.0;
+      F[3] = 0.0;
+   }
+   if (l==1)
+   {
+      F[0] = U;
+      F[1] = 5*J;
+      F[2] = 0.0;
+      F[3] = 0.0;
+   }
+   if (l==2)
+   {
+      F[0] = U;
+      F[1] = 112.0/13.0*J;
+      F[2] =  70.0/13.0*J;
+      F[3] = 0.0;
+   }
+   if (l==3)
+   {
+      F[0] = U;
+      F[1] = 4125.0/346.0*J;
+      F[2] = 5511.0/692.0*J;
+      F[3] = 8151.0/13840*J;
+   }
+
+  // Helper to flatten (i,j,k,l) into a linear index
+    auto idx4 = [lm](int i, int j, int k, int l4) -> std::size_t {
+        // i,j,k,l4 are 0-based within [0..lm-1]
+        return static_cast<std::size_t>(((l4 * lm + k) * lm + j) * lm + i);
+    };
+
+   // call nwpw_gaunt_init(.false.,3)
+   for (auto m1=-l; m1<=l; ++m1)
+   for (auto m2=-l; m2<=l; ++m2)
+   for (auto m3=-l; m3<=l; ++m3)
+   for (auto m4=-l; m4<=l; ++m4)
+   {
+      const int i1 = m1 + l;
+      const int i2 = m2 + l;
+      const int i3 = m3 + l;
+      const int i4 = m4 + l;
+
+      double value = 0.0;
+
+      Paw_gaunt gnt(true, 3);
+      // kk = 1..4  -> k = 0,2,4,6
+      for (int kk = 1; kk <= 4; ++kk) 
+      {
+         const int k = 2 * (kk - 1);
+         double aterm = 0.0;
+
+         for (int q = -k; q <= k; ++q) 
+         {
+            const double g1 = gnt.gaunt(k, q, l, m1, l, m2);
+            const double g2 = gnt.gaunt(k, q, l, m4, l, m3);
+            aterm += g1 * g2;
+         }
+         aterm *= fourpi / static_cast<double>(2 * k + 1);
+
+         value += F[kk - 1] * aterm;
+      }
+ 
+      vmmmm[idx4(i1,i2,i3,i4)] = value;
+   } 
+
+      
+}
 
 /* Constructors */
 
